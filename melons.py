615190d58ad78f0f3f6f577e3ad50173ca0cae8d
ch7oe <chloenixon723@gmail.com>
2025-02-22 16:02:47 -0600

    completed AbstractMelonOrder, DomesticMelonOrder, InternationalMelonOrder

--- melons.py
+++ melons.py
@@ -1,25 +1,33 @@
 """Classes for melon orders."""
 
-
-class DomesticMelonOrder:
-    """A melon order within the USA."""
-
+class AbstractMelonOrder:
+    """An abstract base class that other Melon Orders inherit from."""
+    
+    order_type = None
+    tax = 0
+    
+    
     def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
+        #intialize melon order attributes.
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
+        self.base_price = 5
+        
     def get_total(self):
         """Calculate price, including tax."""
-
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
-
         return total
+        
+
+
+class DomesticMelonOrder(AbstractMelonOrder):    
+    """A melon order within the USA."""
+
+    order_type = "domestic"
+    tax = 0.08
+    
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
@@ -27,26 +35,20 @@ class DomesticMelonOrder:
         self.shipped = True
 
 
-class InternationalMelonOrder:
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
+
+    order_type = 'international'
+    tax = 0.17
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
+        super().__init__(species, qty,)
+
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
+        
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
